0c4380309afc1353e79066e3b9c2b4ca28482a22
liuwei <lvyx620@163.com>
2023-08-05 17:19:34 +0800

    fix:降级vue-cli

--- sub-app-a/src/views/page2/index.vue
+++ sub-app-a/src/views/page2/index.vue
@@ -3,7 +3,7 @@
     <div class="desc">子应用A的page2嵌入第三方页面</div>
     <button @click="$router.go(-1)">返回上一页</button>
     <div>
-      <iframe src="https://www.qiniu.com" width="100%" height="500" frameborder="0" />
+      <iframe src="https://wujicode.cn/xy/app/prod/official/index" width="100%" height="500" frameborder="0" />
     </div>
   </div>
 </template>
--- sub-app-a/vue.config.js
+++ sub-app-a/vue.config.js
@@ -1,5 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
-
 const path = require('path')
 const {
   name
@@ -11,8 +9,7 @@ const resolve = dir => {
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-module.exports = defineConfig({
-  transpileDependencies: true,
+module.exports = {
   outputDir: 'subAppA',
   publicPath: !isProduction ? './' : '/subAppA/', // ./相对路径
   // 打包时不生成.map文件
@@ -28,7 +25,7 @@ module.exports = defineConfig({
       // 把子应用打包成 umd 库格式(必须)
       library: `${name}-[name]`,
       libraryTarget: 'umd',
-      chunkLoadingGlobal: `webpackJsonp_${name}`
+      jsonpFunction: `webpackJsonp_${name}`
     }
   },
   devServer: {
@@ -38,4 +35,4 @@ module.exports = defineConfig({
     },
     // proxy: 'http://192.168.20.132:7002'
   }
-})
+}
